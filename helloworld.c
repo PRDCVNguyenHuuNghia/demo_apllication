6932f67a34b95928bc984978969af7eda946ac9a
PRDCVNguyenHuuNghia <88831714+PRDCVNguyenHuuNghia@users.noreply.github.com>
2021-08-14 23:20:19 +0700

    Delete helloworld.c

--- helloworld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "nghianh_greetings.h"
-int main(void) {
-    char greeting[40];
-    nghianh_get_greeting(greeting);
-    printf("Hello NghiaNH!\n");
-    printf("%s", greeting);
-    return 0;
-}
